e93232b4f4ba92796d346be0ad5911aba329cb58
onlyj <tlawltjr23@gmail.com>
2023-09-05 18:14:16 +0900

    미나 차트 프로그램 시작

---
+++ src/main/java/com/chart/misay/MisayApplication.java
@@ -0,0 +1,13 @@
+package com.chart.misay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MisayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chart/misay/MisayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chart.misay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
